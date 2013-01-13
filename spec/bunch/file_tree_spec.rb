a0a8fe383201d04778c8b83f86b69f06421b2a7a
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-01-12 20:53:00 -0800

    Add specs for FileTree#get_file

--- spec/bunch/file_tree_spec.rb
+++ spec/bunch/file_tree_spec.rb
@@ -8,5 +8,19 @@ module Bunch
       @tree = FileTree.from_hash \
         "a" => {"b" => "foo", "c" => {"d" => "bar"}}, "e" => "baz"
     end
+
+    describe "#get_file" do
+      it "returns an object representing a filename" do
+        @tree.get_file("e").content.must_equal "baz"
+      end
+
+      it "returns an object representing a nested path" do
+        @tree.get_file("a/b").content.must_equal "foo"
+      end
+
+      it "returns an object representing a more nested path" do
+        @tree.get_file("a/c/d").content.must_equal "bar"
+      end
+    end
   end
 end
