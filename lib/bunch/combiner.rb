eefc66617e125c75a9580bce074aaaa342dbbce0
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-03-03 00:14:41 -0800

    Use FileTree#write to implement combining

--- lib/bunch/combiner.rb
+++ lib/bunch/combiner.rb
@@ -9,13 +9,53 @@ module Bunch
     end
 
     def result
-      @tree
+      new_tree = FileTree.new
+
+      @tree.map do |filename, node|
+        case node
+        when File
+          new_tree.write(node.filename, node.content)
+        when FileTree
+          new_tree.write(filename, compile_tree(filename, node))
+        end
+      end
+
+      new_tree
     end
 
     private
 
-    def should_combine?
-      @tree.exist?("_combine")
+    def compile_tree(filename, tree)
+      if should_combine?(tree)
+        generate_file(filename, tree).content
+      else
+        generate_tree(tree).to_hash
+      end
+    end
+
+    def generate_tree(tree)
+      Combiner.new(tree).result
+    end
+
+    def generate_file(filename, tree)
+      contents = []
+
+      tree.each do |filename, node|
+        next if filename == "_combine"
+
+        case node
+        when File
+          contents << node.content
+        when FileTree
+          contents << Combiner.new(node, true).result.content
+        end
+      end
+
+      File.new(filename, contents.join("\n"))
+    end
+
+    def should_combine?(tree)
+      tree.exist?("_combine")
     end
   end
 end
