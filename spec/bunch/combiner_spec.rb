79895823fca54bcd100d8919849f6ea0db5ddc71
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-03-09 18:39:54 -0800

    Refactor combiner tests

--- spec/bunch/combiner_spec.rb
+++ spec/bunch/combiner_spec.rb
@@ -4,79 +4,37 @@ require "spec_helper"
 
 module Bunch
   describe Combiner do
-    def self.it_returns_the_tree_unaltered
-      it "returns the tree unaltered" do
-        @combiner.result.must_equal @combiner.tree
+    def self.scenario(name, input, output)
+      it "combines a tree correctly: #{name}" do
+        combiner = Combiner.new FileTree.from_hash(input)
+        result   = combiner.result.to_hash
+
+        result.must_equal output
       end
     end
 
-    describe "given a tree with one JavaScript file" do
-      before do
-        @combiner = Combiner.new FileTree.from_hash("a.js" => "hello;")
-      end
+    scenario "one JavaScript file",
+      {"a.js" => "hello;"},
+      {"a.js" => "hello;"}
 
-      it_returns_the_tree_unaltered
-    end
+    scenario "two JavaScript files in a directory",
+      {"a" => {"b.js" => "hello;", "c.js" => "goodbye;"}},
+      {"a" => {"b.js" => "hello;", "c.js" => "goodbye;"}}
 
-    describe "given a tree with two JavaScript files in a directory" do
-      before do
-        @tree     = {"a" => {"b.js" => "hello;", "c.js" => "goodbye;"}}
-        @combiner = Combiner.new FileTree.from_hash(@tree)
-      end
+    scenario "two JavaScript files and a _combine file",
+      {"a" => {"_combine" => "", "b.js" => "hello;", "c.js" => "goodbye;"}},
+      {"a.js" => "hello;\ngoodbye;"}
 
-      it_returns_the_tree_unaltered
-    end
-
-    describe "given a tree with two JavaScript files and a _combine file" do
-      before do
-        @tree = {
-          "a" => {
-            "_combine" => "", "b.js" => "hello;", "c.js" => "goodbye;"
-          }
-        }
-        @combiner = Combiner.new FileTree.from_hash(@tree)
-      end
-
-      it "returns a tree with the files concatenated" do
-        result = @combiner.result
-        hash = result.to_hash
-
-        hash.values.must_equal ["hello;\ngoodbye;"]
-
-        result.get(hash.keys[0]).content.must_equal "hello;\ngoodbye;"
-      end
-
-      it "gives the result the correct name, extension, and mime type" do
-        result = @combiner.result
-        result.to_hash.keys.must_equal ["a.js"]
-        file = result.get("a.js")
-        file.mime_type.must_equal "application/javascript"
-      end
-    end
-
-    describe "given a nested tree" do
-      before do
-        @tree = {
-          "a" => {
-            "b" => {
-              "_combine" => "", "c.js" => "hello;", "d.js" => "goodbye;"
-            },
-            "e.js" => "and_another_thing;"
-          }
-        }
-        @combiner = Combiner.new FileTree.from_hash(@tree)
-      end
-
-      it "returns a tree with the files concatenated" do
-        result = @combiner.result
-        hash = result.to_hash
-
-        # todo: fix the extensions
-        hash.must_equal "a" => {
-          "b.js" => "hello;\ngoodbye;",
-          "e.js" => "and_another_thing;"
-        }
-      end
-    end
+    scenario "a more complex tree of JavaScript files",
+      {"a" => {
+         "b" => {
+           "_combine" => "", "c.js" => "hello;", "d.js" => "goodbye;"
+         },
+         "e.js" => "and_another_thing;"
+      }},
+      {"a" => {
+         "b.js" => "hello;\ngoodbye;",
+         "e.js" => "and_another_thing;"
+      }}
   end
 end
