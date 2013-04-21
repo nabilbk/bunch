04fe33cfc738eae7ddee3fe6edb326e2cea6f3e3
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-04-21 00:51:12 -0700

    Remove dead stuff

--- lib/bunch.rb
+++ lib/bunch.rb
@@ -2,15 +2,6 @@
 
 require "mime/types"
 require "bunch/version"
-
-module Bunch
-  def self.catalog
-    @catalog ||= Catalog.new
-  end
-end
-
-require "bunch/catalog"
 require "bunch/combiner"
 require "bunch/file"
 require "bunch/file_tree"
-require "bunch/path"
