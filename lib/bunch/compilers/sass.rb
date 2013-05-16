c48e8c7ba94844c42668043801379610ad1d2b8a
Ryan Fitzgerald <rwfitzge@gmail.com>
2013-05-15 23:55:20 -0700

    Use self shunt as importer

--- lib/bunch/compilers/sass.rb
+++ lib/bunch/compilers/sass.rb
@@ -15,46 +15,31 @@ module Bunch
       end
 
       def content
-        syntax = @file.path.end_with?("scss") ? :scss : :sass
-        engine = ::Sass::Engine.new(
-          @file.content,
-          syntax: syntax,
-          filename: @abs_path,
-          importer: Importer.new(@tree)
-        )
-        engine.render
+        new_engine(@file.content, @file.path).render
       end
 
-      class Importer
-        def initialize(tree)
-          @tree = tree
-        end
+      def new_engine(content, path)
+        syntax = path.end_with?("scss") ? :scss : :sass
+        ::Sass::Engine.new(
+          content, syntax: syntax, filename: path, importer: self)
+      end
 
-        def find_relative(path, base, options)
-          abs = ::File.expand_path("../#{path}", "/#{base}")[1..-1]
-          find(abs, options)
-        end
+      ## Sass importer interface ##
 
-        def find(path, options)
-          syntax = path.end_with?("scss") ? :scss : :sass
-          ::Sass::Engine.new(
-            @tree.get(path).content,
-            syntax: syntax,
-            filename: path,
-            importer: self
-          )
-        end
+      def find_relative(path, base, options)
+        abs = ::File.expand_path("../#{path}", "/#{base}")[1..-1]
+        find(abs, options)
+      end
 
-        def mtime(path, options)
-        end
+      def find(path, options)
+        new_engine(@tree.get(path).content, path)
+      end
 
-        def key(path, options)
-          ["Bunch:#{::File.dirname(path)}", ::File.basename(path)]
-        end
+      def mtime(path, options)
+      end
 
-        def to_s
-          "(Bunch file tree)"
-        end
+      def key(path, options)
+        ["Bunch:#{::File.dirname(path)}", ::File.basename(path)]
       end
     end
   end
